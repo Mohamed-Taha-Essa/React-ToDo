fa40a43044962998647e9caae4debec3ef610b1b
Mohamed Abdallah Essa <www.taha.166@gmail.com>
2024-07-09 15:00:36 -0700

    connect teact with api using useeffect hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-
+import { useEffect, useState } from "react";
 import './App.css'
 import CreateToDo from './components/CreateToDo'
 import ToDos from './components/ToDos'
 
 function App() {
+
+  const [todos ,settodos] =useState([])
+
+  useEffect(()=>{
+    fetch('http://127.0.0.1:8000/todo')
+    .then(response=>response.json())
+    .then(data =>settodos(data))
+  })
 
   return (
     
@@ -12,7 +20,7 @@ function App() {
         <div className="row ">
           <div className="col-lg-8 mx-auto">
             <CreateToDo/>
-            <ToDos/>
+            <ToDos todos={todos} settodos={settodos}/>
           </div>
         </div>
       
--- src/components/ToDos.jsx
+++ src/components/ToDos.jsx
@@ -1,23 +1,14 @@
 import { useState } from "react"
 
-const ToDos =()=>
+const ToDos =({todos,settodos})=>
     {
-      const [todo ,settodos] =useState([
-        {tilte:'todo1' ,status :'DONE'},
-        {tilte:'todo2' ,status :'INPROGRESS'},
-        {tilte:'todo3' ,status :'INPROGRESS'},
-        {tilte:'todo4' ,status :'DONE'},
-        {tilte:'todo5' ,status :'INPROGRESS'},
-
-      ])
-    
-    
+     
         return(
             <div className="container mt-4">
                 <ul className="list-group">
-                    {todo.map((todo,index)=>(
+                    {todos.map((todo,index)=>(
                         <li key={index} className="list-group-item d-flex justify-content-between">
-                            <span>{todo.tilte}</span>
+                            <span>{todo.title}</span>
                             <button className="btn btn-primary border"> {todo.status}</button>
 
                         </li>
